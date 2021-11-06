0a6ee05f202b2b0e8c4350a1d08328eca8acf886
Eric <1247106503@qq.com>
2021-11-06 14:29:47 +0800

    练习代码

--- review/re_415.java
+++ review/re_415.java
@@ -1,3 +1,5 @@
+import java.util.jar.JarEntry;
+
 /**
  * @author chenxin
  * @create 2021-11-06 13:33
@@ -10,6 +12,22 @@ public class re_415 {
 
 class Solution_415 {
     public String addStrings(String num1, String num2) {
-        return "0";
+        StringBuilder res = new StringBuilder();
+        int n = num1.length() - 1;
+        int m = num2.length() - 1;
+        int carry = 0;
+        while (n >= 0 || m >= 0) {
+            int n1 = n >= 0 ? num1.charAt(n) - '0' : 0;
+            int n2 = m >= 0 ? num2.charAt(m) - '0' : 0;
+            int temp = n1 + n2 + carry;
+            carry = temp / 10;
+            res.append(temp % 10);
+            n--;
+            m--;
+        }
+        if (carry == 1) {
+            res.append(1);
+        }
+        return res.reverse().toString();
     }
 }
